21069b326a7f7b4cb44c1630cfd700e08b1d2410
jsgroth <1137683+jsgroth@users.noreply.github.com>
2023-03-09 22:06:57 -0600

    SET and RES tests

--- jgb-core/src/cpu/tests/singlebit.rs
+++ jgb-core/src/cpu/tests/singlebit.rs
@@ -69,3 +69,121 @@ fn test_bit_indirect_hl() {
         },
     );
 }
+
+#[test]
+fn set_bit_register() {
+    for r in ALL_REGISTERS {
+        let ld = 0x06 | (r.to_opcode_bits() << 3);
+        let ld = format!("{ld:02x}");
+
+        for bit in 0..8 {
+            let opcode = 0xC0 | (bit << 3) | r.to_opcode_bits();
+            let opcode = format!("CB{opcode:02x}");
+
+            let mut expected_state = ExpectedState::empty();
+            set_in_state(&mut expected_state, r, 1 << bit);
+            expected_state.f = Some(0x00);
+            run_test(
+                // LD <r>, 0x00; SET <b>, <r>
+                &format!("{ld}00{opcode}"),
+                &expected_state,
+            );
+
+            let mut expected_state = ExpectedState::empty();
+            set_in_state(&mut expected_state, r, 0xFF);
+            expected_state.f = Some(0x00);
+            run_test(
+                // LD <r>, 0xFF; SET <b>, <r>
+                &format!("{ld}FF{opcode}"),
+                &expected_state,
+            );
+        }
+    }
+}
+
+#[test]
+fn set_bit_indirect_hl() {
+    for bit in 0..8 {
+        let opcode = 0xC6 | (bit << 3);
+        let opcode = format!("CB{opcode:02x}");
+
+        run_test(
+            // LD HL, 0xC723; LD (HL), 0x00; SET <b>, (HL)
+            &format!("2123C73600{opcode}"),
+            &ExpectedState {
+                memory: hash_map! { 0xC723: 1 << bit },
+                f: Some(0x00),
+                ..ExpectedState::empty()
+            },
+        );
+
+        run_test(
+            // LD HL, 0xC723; LD (HL), 0xFF; SET <b>, (HL)
+            &format!("2123C736FF{opcode}"),
+            &ExpectedState {
+                memory: hash_map! { 0xC723: 0xFF },
+                f: Some(0x00),
+                ..ExpectedState::empty()
+            },
+        );
+    }
+}
+
+#[test]
+fn reset_bit_register() {
+    for r in ALL_REGISTERS {
+        let ld = 0x06 | (r.to_opcode_bits() << 3);
+        let ld = format!("{ld:02x}");
+
+        for bit in 0..8 {
+            let opcode = 0x80 | (bit << 3) | r.to_opcode_bits();
+            let opcode = format!("CB{opcode:02x}");
+
+            let mut expected_state = ExpectedState::empty();
+            set_in_state(&mut expected_state, r, 0x00);
+            expected_state.f = Some(0x00);
+            run_test(
+                // LD <r>, 0x00; RES <b>, <r>
+                &format!("{ld}00{opcode}"),
+                &expected_state,
+            );
+
+            let mut expected_state = ExpectedState::empty();
+            set_in_state(&mut expected_state, r, 0xFF & !(1 << bit));
+            expected_state.f = Some(0x00);
+            run_test(
+                // LD <r>, 0xFF; RES <b>, <r>
+                &format!("{ld}FF{opcode}"),
+                &expected_state,
+            );
+        }
+    }
+}
+
+#[test]
+fn reset_bit_indirect_hl() {
+    for bit in 0..8 {
+        let opcode = 0x86 | (bit << 3);
+        let opcode = format!("CB{opcode:02x}");
+
+        run_test(
+            // LD HL, 0xC5EB; LD (HL), 0x00; RES <b>, (HL)
+            &format!("21EBC53600{opcode}"),
+            &ExpectedState {
+                memory: hash_map! { 0xC5EB: 0x00 },
+                f: Some(0x00),
+                ..ExpectedState::empty()
+            },
+        );
+
+        run_test(
+            // LD HL, 0xC5EB; LD (HL), 0xFF; RES <b>, (HL)
+            &format!("21EBC536FF{opcode}"),
+            &ExpectedState {
+                memory: hash_map! { 0xC5EB: 0xFF & !(1 << bit) },
+                f: Some(0x00),
+                ..ExpectedState::empty()
+            },
+        );
+    }
+}
