7be0818754c24fd5ebb461d73712ac4dd70a476a
jsgroth <1137683+jsgroth@users.noreply.github.com>
2023-03-09 23:04:06 -0600

    conditional jump tests

--- jgb-core/src/cpu/tests/controlflow.rs
+++ jgb-core/src/cpu/tests/controlflow.rs
@@ -52,3 +52,153 @@ fn jump_hl() {
         },
     );
 }
+
+#[test]
+fn conditional_jump_nz() {
+    run_test(
+        concat!(
+            "06AA",   // 0x0150: LD B, 0xAA
+            "3E00",   // 0x0152: LD A, 0x00
+            "FE00",   // 0x0154: CP 0x00,
+            "C25B01", // 0x0156: JP NZ, 0x015B
+            "06BB",   // 0x0159: LD B, 0xBB
+            "0ECC",   // 0x015B: LD C, 0xCC
+        ),
+        &ExpectedState {
+            a: Some(0x00),
+            b: Some(0xBB),
+            c: Some(0xCC),
+            f: Some(0xC0),
+            ..ExpectedState::empty()
+        },
+    );
+
+    run_test(
+        concat!(
+            "06AA",   // 0x0150: LD B, 0xAA
+            "3E01",   // 0x0152: LD A, 0x01,
+            "FE00",   // 0x0154: CP 0x00,
+            "C25B01", // 0x0156: JP NZ, 0x015B
+            "06BB",   // 0x0159: LD B, 0xBB
+            "0ECC",   // 0x015B: LD C, 0xCC
+        ),
+        &ExpectedState {
+            a: Some(0x01),
+            b: Some(0xAA),
+            c: Some(0xCC),
+            f: Some(0x40),
+            ..ExpectedState::empty()
+        },
+    );
+}
+
+#[test]
+fn conditional_jump_z() {
+    run_test(
+        concat!(
+            "06AA",   // 0x0150: LD B, 0xAA
+            "3E00",   // 0x0152: LD A, 0x00
+            "FE00",   // 0x0154: CP 0x00,
+            "CA5B01", // 0x0156: JP Z, 0x015B
+            "06BB",   // 0x0159: LD B, 0xBB
+            "0ECC",   // 0x015B: LD C, 0xCC
+        ),
+        &ExpectedState {
+            a: Some(0x00),
+            b: Some(0xAA),
+            c: Some(0xCC),
+            f: Some(0xC0),
+            ..ExpectedState::empty()
+        },
+    );
+
+    run_test(
+        concat!(
+            "06AA",   // 0x0150: LD B, 0xAA
+            "3E01",   // 0x0152: LD A, 0x01,
+            "FE00",   // 0x0154: CP 0x00,
+            "CA5B01", // 0x0156: JP Z, 0x015B
+            "06BB",   // 0x0159: LD B, 0xBB
+            "0ECC",   // 0x015B: LD C, 0xCC
+        ),
+        &ExpectedState {
+            a: Some(0x01),
+            b: Some(0xBB),
+            c: Some(0xCC),
+            f: Some(0x40),
+            ..ExpectedState::empty()
+        },
+    );
+}
+
+#[test]
+fn conditional_jump_nc() {
+    run_test(
+        concat!(
+            "06AA",   // 0x0150: LD B, 0xAA
+            "37",     // 0x0152: SCF
+            "3F",     // 0x0153: CCF
+            "D25901", // 0x0154: JP NC, 0x0159
+            "06BB",   // 0x0157: LD B, 0xBB
+            "0ECC",   // 0x0159: LD C, 0xCC
+        ),
+        &ExpectedState {
+            b: Some(0xAA),
+            c: Some(0xCC),
+            f: Some(0x00),
+            ..ExpectedState::empty()
+        },
+    );
+
+    run_test(
+        concat!(
+            "06AA",   // 0x0150: LD B, 0xAA
+            "37",     // 0x0152: SCF
+            "D25801", // 0x0153: JP NC, 0x0158
+            "06BB",   // 0x0156: LD B, 0xBB
+            "0ECC",   // 0x0158: LD C, 0xCC
+        ),
+        &ExpectedState {
+            b: Some(0xBB),
+            c: Some(0xCC),
+            f: Some(0x10),
+            ..ExpectedState::empty()
+        },
+    );
+}
+
+#[test]
+fn conditional_jump_c() {
+    run_test(
+        concat!(
+            "06AA",   // 0x0150: LD B, 0xAA
+            "37",     // 0x0152: SCF
+            "3F",     // 0x0153: CCF
+            "DA5901", // 0x0154: JP C, 0x0159
+            "06BB",   // 0x0157: LD B, 0xBB
+            "0ECC",   // 0x0159: LD C, 0xCC
+        ),
+        &ExpectedState {
+            b: Some(0xBB),
+            c: Some(0xCC),
+            f: Some(0x00),
+            ..ExpectedState::empty()
+        },
+    );
+
+    run_test(
+        concat!(
+            "06AA",   // 0x0150: LD B, 0xAA
+            "37",     // 0x0152: SCF
+            "DA5801", // 0x0153: JP C, 0x0158
+            "06BB",   // 0x0156: LD B, 0xBB
+            "0ECC",   // 0x0158: LD C, 0xCC
+        ),
+        &ExpectedState {
+            b: Some(0xAA),
+            c: Some(0xCC),
+            f: Some(0x10),
+            ..ExpectedState::empty()
+        },
+    );
+}
