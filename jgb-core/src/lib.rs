5b4ddba7d3cd519d9cc2afd4ea6b39e0ea6b2e29
jsgroth <1137683+jsgroth@users.noreply.github.com>
2023-03-24 14:25:22 -0500

    remove commented-out code

--- jgb-core/src/lib.rs
+++ jgb-core/src/lib.rs
@@ -35,44 +35,5 @@ pub fn run(
 
     eventloop::run(emulation_state, sdl_state)?;
 
-    //
-    // let texture_creator = sdl_state.canvas.texture_creator();
-    // let mut window_texture =
-    //     texture_creator.create_texture_streaming(PixelFormatEnum::RGB24, 160, 144)?;
-    //
-    // 'running: loop {
-    //     for event in sdl_state.event_pump.poll_iter() {
-    //         match event {
-    //             Event::Quit { .. }
-    //             | Event::KeyDown {
-    //                 keycode: Some(Keycode::Escape),
-    //                 ..
-    //             } => {
-    //                 break 'running;
-    //             }
-    //             _ => {}
-    //         }
-    //     }
-    //
-    //     sdl_state.canvas.clear();
-    //     window_texture.with_lock(None, |pixels, pitch| {
-    //         for i in 0..144 {
-    //             for j in 0..160 {
-    //                 pixels[i * pitch + j * 3..i * pitch + (j + 1) * 3].copy_from_slice(&[
-    //                     rand::random(),
-    //                     rand::random(),
-    //                     rand::random(),
-    //                 ]);
-    //             }
-    //         }
-    //     })?;
-    //     sdl_state.canvas.copy(&window_texture, None, None)?;
-    //     sdl_state.canvas.present();
-    //
-    //     thread::sleep(Duration::from_millis(10));
-    // }
-
-    // cpu::run(emulation_state)?;
-
     Ok(())
 }
