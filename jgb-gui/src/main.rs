84179f8183107c0f13572383bc247ecc8d61afb4
jsgroth <1137683+jsgroth@users.noreply.github.com>
2023-03-31 19:59:23 -0500

    make the GUI use env_logger instead of tracing_subscriber, the latter tanks performance because of all the log::trace! calls

--- jgb-gui/src/main.rs
+++ jgb-gui/src/main.rs
@@ -1,8 +1,9 @@
 use eframe::NativeOptions;
+use env_logger::Env;
 use jgb_gui::JgbApp;
 
 fn main() -> eframe::Result<()> {
-    tracing_subscriber::fmt::init();
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
     eframe::run_native(
         "jgb",
