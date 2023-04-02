eaa83b3affc5b303ece1004a7ce4123196a8c5c4
jsgroth <1137683+jsgroth@users.noreply.github.com>
2023-04-02 15:45:18 -0500

    clean up GUI config code a bit, remove redundant functions and remove redundant defaults between Default::default() and #[serde(default)] declarations

--- jgb-gui/src/app/config.rs
+++ jgb-gui/src/app/config.rs
@@ -17,7 +17,7 @@ impl Default for FullscreenMode {
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct AppConfig {
-    #[serde(default = "default_vsync_enabled")]
+    #[serde(default = "true_fn")]
     pub vsync_enabled: bool,
 
     #[serde(default)]
@@ -26,16 +26,16 @@ pub struct AppConfig {
     #[serde(default)]
     pub fullscreen_mode: FullscreenMode,
 
-    #[serde(default = "default_force_integer_scaling")]
+    #[serde(default = "true_fn")]
     pub force_integer_scaling: bool,
 
-    #[serde(default = "default_audio_enabled")]
+    #[serde(default = "true_fn")]
     pub audio_enabled: bool,
 
-    #[serde(default = "default_audio_sync_enabled")]
+    #[serde(default = "true_fn")]
     pub audio_sync_enabled: bool,
 
-    #[serde(default = "default_audio_60hz_hack_enabled")]
+    #[serde(default = "true_fn")]
     pub audio_60hz_hack_enabled: bool,
 
     #[serde(default = "default_window_width")]
@@ -47,23 +47,8 @@ pub struct AppConfig {
     pub rom_search_dir: Option<String>,
 }
 
-fn default_vsync_enabled() -> bool {
-    true
-}
-
-fn default_force_integer_scaling() -> bool {
-    true
-}
-
-fn default_audio_enabled() -> bool {
-    true
-}
-
-fn default_audio_sync_enabled() -> bool {
-    true
-}
-
-fn default_audio_60hz_hack_enabled() -> bool {
+// #[serde(default)] requires a function
+fn true_fn() -> bool {
     true
 }
 
@@ -77,18 +62,9 @@ fn default_window_height() -> u32 {
 
 impl Default for AppConfig {
     fn default() -> Self {
-        Self {
-            vsync_enabled: default_vsync_enabled(),
-            launch_in_fullscreen: bool::default(),
-            fullscreen_mode: FullscreenMode::default(),
-            force_integer_scaling: default_force_integer_scaling(),
-            audio_enabled: default_audio_enabled(),
-            audio_sync_enabled: default_audio_sync_enabled(),
-            audio_60hz_hack_enabled: default_audio_60hz_hack_enabled(),
-            window_width: default_window_width(),
-            window_height: default_window_height(),
-            rom_search_dir: Option::default(),
-        }
+        // Hack to ensure that defaults are always identical between Default::default() and the
+        // serde defaults
+        toml::from_str("").expect("deserializing AppConfig from empty string should always succeed")
     }
 }
 
@@ -115,5 +91,16 @@ impl AppConfig {
             .with_context(|| format!("error writing app config to '{path:?}'"))?;
 
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn app_config_default_does_not_panic() {
+        let app_config = AppConfig::default();
+        assert!(app_config.vsync_enabled);
     }
 }
