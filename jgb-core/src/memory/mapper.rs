13f1b15e687c4d340719cc79fe57ec59a5bb74f4
jsgroth <1137683+jsgroth@users.noreply.github.com>
2023-04-14 16:41:20 -0500

    move real-time clock code to a submodule

--- jgb-core/src/memory/mapper.rs
+++ jgb-core/src/memory/mapper.rs
@@ -1,10 +1,13 @@
+mod mbc3;
 mod mbc7;
 
 use crate::memory::address;
 use crate::memory::mapper::mbc7::Mbc7Eeprom;
 use serde::{Deserialize, Serialize};
 use std::fmt::Formatter;
-use std::time::{Duration, SystemTime};
+use std::time::SystemTime;
+
+pub(crate) use mbc3::RealTimeClock;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub(crate) enum MapperType {
@@ -14,111 +17,6 @@ pub(crate) enum MapperType {
     MBC3,
     MBC5,
     MBC7,
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
-struct RtcTime {
-    nanos: u32,
-    seconds: u8,
-    minutes: u8,
-    hours: u8,
-    days: u16,
-    day_overflow_flag: bool,
-}
-
-impl RtcTime {
-    fn new() -> Self {
-        Self {
-            nanos: 0,
-            seconds: 0,
-            minutes: 0,
-            hours: 0,
-            days: 0,
-            day_overflow_flag: false,
-        }
-    }
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-pub(crate) struct RealTimeClock {
-    last_update: SystemTime,
-    current_time: RtcTime,
-    latched_time: Option<RtcTime>,
-    pre_latched: bool,
-    halted: bool,
-}
-
-impl RealTimeClock {
-    fn new(now: SystemTime) -> Self {
-        Self {
-            last_update: now,
-            current_time: RtcTime::new(),
-            latched_time: None,
-            pre_latched: false,
-            halted: false,
-        }
-    }
-
-    fn update(&mut self, now: SystemTime) {
-        let since = now.duration_since(self.last_update).unwrap_or_else(|err| {
-            log::error!(
-                "Time has gone backwards: last_update={:?}, now={now:?}: {err}",
-                self.last_update
-            );
-            Duration::from_secs(0)
-        });
-
-        self.last_update = now;
-
-        if self.halted {
-            return;
-        }
-
-        let nanos = u128::from(self.current_time.nanos) + since.as_nanos();
-        self.current_time.nanos = (nanos % 1_000_000_000) as u32;
-        if nanos < 1_000_000_000 {
-            return;
-        }
-
-        let seconds = u64::from(self.current_time.seconds) + (nanos / 1_000_000_000) as u64;
-        self.current_time.seconds = (seconds % 60) as u8;
-        if seconds < 60 {
-            return;
-        }
-
-        let minutes = u64::from(self.current_time.minutes) + (seconds / 60);
-        self.current_time.minutes = (minutes % 60) as u8;
-        if minutes < 60 {
-            return;
-        }
-
-        let hours = u64::from(self.current_time.hours) + (minutes / 60);
-        self.current_time.hours = (hours % 24) as u8;
-        if hours < 24 {
-            return;
-        }
-
-        let days = u64::from(self.current_time.days) + (hours / 24);
-        self.current_time.days = (days % 512) as u16;
-        if days < 512 {
-            return;
-        }
-
-        self.current_time.day_overflow_flag = true;
-    }
-
-    fn process_register_write(&mut self, value: u8) {
-        if value == 0x01 && self.pre_latched {
-            self.pre_latched = false;
-            self.latched_time = Some(self.current_time);
-        } else if value == 0x00 {
-            self.pre_latched = true;
-            self.latched_time = None;
-        } else {
-            self.pre_latched = false;
-            self.latched_time = None;
-        }
-    }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -520,22 +418,7 @@ impl Mapper {
             return None;
         };
 
-        let time = real_time_clock
-            .latched_time
-            .unwrap_or(real_time_clock.current_time);
-
-        match ram_bank_number {
-            0x08 => Some(time.seconds),
-            0x09 => Some(time.minutes),
-            0x0A => Some(time.hours),
-            0x0B => Some((time.days & 0xFF) as u8),
-            0x0C => Some(
-                (u8::from(time.day_overflow_flag) << 7)
-                    | (u8::from(real_time_clock.halted) << 6)
-                    | (time.days >> 8) as u8,
-            ),
-            _ => None,
-        }
+        real_time_clock.handle_ram_read(*ram_bank_number)
     }
 
     pub(crate) fn write_ram_addressed_register(&mut self, value: u8) {
@@ -548,29 +431,7 @@ impl Mapper {
             return;
         };
 
-        match ram_bank_number {
-            0x08 => {
-                real_time_clock.current_time.seconds = value;
-                real_time_clock.current_time.nanos = 0;
-            }
-            0x09 => {
-                real_time_clock.current_time.minutes = value;
-            }
-            0x0A => {
-                real_time_clock.current_time.hours = value;
-            }
-            0x0B => {
-                real_time_clock.current_time.days =
-                    (real_time_clock.current_time.days & 0xFF00) | u16::from(value);
-            }
-            0x0C => {
-                real_time_clock.current_time.days =
-                    (real_time_clock.current_time.days & 0x00FF) | (u16::from(value & 0x01) << 8);
-                real_time_clock.halted = value & 0x40 != 0;
-                real_time_clock.current_time.day_overflow_flag = value & 0x80 != 0;
-            }
-            _ => {}
-        }
+        real_time_clock.handle_ram_write(*ram_bank_number, value);
     }
 
     pub(crate) fn update_rtc(&mut self) {
@@ -661,7 +522,7 @@ mod tests {
     #[test]
     fn mbc1_mapper_rom_small() {
         // 256KB ROM
-        let mut mapper = Mapper::new(MapperType::MBC1, mapper_features(), None, 1 << 18, 0);
+        let mut mapper = Mapper::new(MapperType::MBC1, mapper_features(), None, 1 << 18, 0, None);
 
         assert_eq!(0x0000, mapper.map_rom_address(0x0000));
         assert_eq!(0x3FFF, mapper.map_rom_address(0x3FFF));
@@ -700,7 +561,7 @@ mod tests {
     #[test]
     fn mbc1_mapper_rom_large() {
         // 2MB ROM
-        let mut mapper = Mapper::new(MapperType::MBC1, mapper_features(), None, 1 << 21, 0);
+        let mut mapper = Mapper::new(MapperType::MBC1, mapper_features(), None, 1 << 21, 0, None);
 
         assert_eq!(0x0000, mapper.map_rom_address(0x0000));
         assert_eq!(0x3FFF, mapper.map_rom_address(0x3FFF));
@@ -731,7 +592,14 @@ mod tests {
     #[test]
     fn mbc1_mapper_ram() {
         // 256KB ROM, 8KB RAM
-        let mut mapper = Mapper::new(MapperType::MBC1, mapper_features(), None, 1 << 18, 8192);
+        let mut mapper = Mapper::new(
+            MapperType::MBC1,
+            mapper_features(),
+            None,
+            1 << 18,
+            8192,
+            None,
+        );
 
         // Enable RAM
         mapper.write_rom_address(0x0000, 0x0A);
---
+++ jgb-core/src/memory/mapper/mbc3.rs
@@ -0,0 +1,149 @@
+use serde::{Deserialize, Serialize};
+use std::time::{Duration, SystemTime};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
+struct RtcTime {
+    nanos: u32,
+    seconds: u8,
+    minutes: u8,
+    hours: u8,
+    days: u16,
+    day_overflow_flag: bool,
+}
+
+impl RtcTime {
+    fn new() -> Self {
+        Self {
+            nanos: 0,
+            seconds: 0,
+            minutes: 0,
+            hours: 0,
+            days: 0,
+            day_overflow_flag: false,
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub(crate) struct RealTimeClock {
+    last_update: SystemTime,
+    current_time: RtcTime,
+    latched_time: Option<RtcTime>,
+    pre_latched: bool,
+    halted: bool,
+}
+
+impl RealTimeClock {
+    pub(crate) fn new(now: SystemTime) -> Self {
+        Self {
+            last_update: now,
+            current_time: RtcTime::new(),
+            latched_time: None,
+            pre_latched: false,
+            halted: false,
+        }
+    }
+
+    pub(crate) fn update(&mut self, now: SystemTime) {
+        let since = now.duration_since(self.last_update).unwrap_or_else(|err| {
+            log::error!(
+                "Time has gone backwards: last_update={:?}, now={now:?}: {err}",
+                self.last_update
+            );
+            Duration::from_secs(0)
+        });
+
+        self.last_update = now;
+
+        if self.halted {
+            return;
+        }
+
+        let nanos = u128::from(self.current_time.nanos) + since.as_nanos();
+        self.current_time.nanos = (nanos % 1_000_000_000) as u32;
+        if nanos < 1_000_000_000 {
+            return;
+        }
+
+        let seconds = u64::from(self.current_time.seconds) + (nanos / 1_000_000_000) as u64;
+        self.current_time.seconds = (seconds % 60) as u8;
+        if seconds < 60 {
+            return;
+        }
+
+        let minutes = u64::from(self.current_time.minutes) + (seconds / 60);
+        self.current_time.minutes = (minutes % 60) as u8;
+        if minutes < 60 {
+            return;
+        }
+
+        let hours = u64::from(self.current_time.hours) + (minutes / 60);
+        self.current_time.hours = (hours % 24) as u8;
+        if hours < 24 {
+            return;
+        }
+
+        let days = u64::from(self.current_time.days) + (hours / 24);
+        self.current_time.days = (days % 512) as u16;
+        if days < 512 {
+            return;
+        }
+
+        self.current_time.day_overflow_flag = true;
+    }
+
+    pub(crate) fn process_register_write(&mut self, value: u8) {
+        if value == 0x01 && self.pre_latched {
+            self.pre_latched = false;
+            self.latched_time = Some(self.current_time);
+        } else if value == 0x00 {
+            self.pre_latched = true;
+            self.latched_time = None;
+        } else {
+            self.pre_latched = false;
+            self.latched_time = None;
+        }
+    }
+
+    pub(crate) fn handle_ram_read(&self, ram_bank_number: u8) -> Option<u8> {
+        let time = self.latched_time.unwrap_or(self.current_time);
+
+        match ram_bank_number {
+            0x08 => Some(time.seconds),
+            0x09 => Some(time.minutes),
+            0x0A => Some(time.hours),
+            0x0B => Some((time.days & 0xFF) as u8),
+            0x0C => Some(
+                (u8::from(time.day_overflow_flag) << 7)
+                    | (u8::from(self.halted) << 6)
+                    | (time.days >> 8) as u8,
+            ),
+            _ => None,
+        }
+    }
+
+    pub(crate) fn handle_ram_write(&mut self, ram_bank_number: u8, value: u8) {
+        match ram_bank_number {
+            0x08 => {
+                self.current_time.seconds = value;
+                self.current_time.nanos = 0;
+            }
+            0x09 => {
+                self.current_time.minutes = value;
+            }
+            0x0A => {
+                self.current_time.hours = value;
+            }
+            0x0B => {
+                self.current_time.days = (self.current_time.days & 0xFF00) | u16::from(value);
+            }
+            0x0C => {
+                self.current_time.days =
+                    (self.current_time.days & 0x00FF) | (u16::from(value & 0x01) << 8);
+                self.halted = value & 0x40 != 0;
+                self.current_time.day_overflow_flag = value & 0x80 != 0;
+            }
+            _ => {}
+        }
+    }
+}
